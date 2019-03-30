fab67cefc44bf2e0403ba58fc4121bb99d0a564b
Nuno Machado <nunom4chado@gmail.com>
2019-03-30 17:31:25 +0000

    fixes responsive layout

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <?php wp_head(); ?>
 </head>
 <body>
