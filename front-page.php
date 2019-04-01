fbb6f83c96f1d423a9fbbdd6d5e47a3c11e89d49
Nuno Machado <nunom4chado@gmail.com>
2019-04-01 12:14:21 +0100

    add content to front-page and add basic wp functions to index.php

--- front-page.php
+++ front-page.php
@@ -1,21 +1,12 @@
-<?php 
-    get_header(); ?>
+<?php get_header(); ?>
 
 
     <div class="page-layout">
       <div class="section-normal">
 
-        <?php while(have_posts()) {
-            the_post(); ?>
-
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <?php the_content(); ?>
-            <hr />
-
-        <?php } ?>
+        <p>im the front-page.php</p>
 
         </div>
     </div>
 
-    <?php get_footer();
-?>
+<?php get_footer(); ?>
