b366d190a4b94d103628b1fc0fcd3d6f30c2c342
Nuno Machado <nunom4chado@gmail.com>
2019-07-19 15:48:20 +0100

    adds category link to alimentos page

--- single-alimento.php
+++ single-alimento.php
@@ -8,7 +8,24 @@
             <div class="ne-sub-header ne-sub-header__alimento">
                 <div class="section-normal">
                     <h1 class="ne-sub-header__title"><?php the_title(); ?></h1>
-                    <p class="ne-sub-header__category">Define category here</p>
+                    <p class="ne-sub-header__category">
+                        <?php
+                        $terms = get_terms( 'categoria_alimento' );
+                                
+                        foreach ( $terms as $term ) {
+                        
+                            // The $term is an object, so we don't need to specify the $taxonomy.
+                            $term_link = get_term_link( $term );
+                            
+                            // If there was an error, continue to the next term.
+                            if ( is_wp_error( $term_link ) ) {
+                                continue;
+                            }
+                        
+                            // We successfully got a link. Print it out.
+                            echo '<a class="ne-link--white" href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
+                        } ?>
+                    </p>
                 </div>
             </div>
             <div class="section-normal breadcrumb-container">
@@ -23,14 +40,11 @@
                     <div class="column-with-sidebar__main">
                         <!-- Post section -->
                         <article class="ne-blog-post__container">
-                            <p class="ne-blog-post__category"><?php echo get_the_category_list(', '); ?></p>
                             <?php
                                 if ( has_post_thumbnail() ) {
                                     the_post_thumbnail();
                                 } 
                             ?>
-                            <p>sazonalidade</p>
-                            <?php the_field('sazonalidade'); ?>
                             <div class="ne-blog-post__content"><?php the_field('conteudo_principal'); ?></div>
 
                             <!-- 
