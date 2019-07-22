f8043c83133434309c93a0f015798984e4964c6c
Nuno Machado <nunom4chado@gmail.com>
2019-07-22 16:52:18 +0100

    adds related nutrientes on sidebar

--- single-nutriente.php
+++ single-nutriente.php
@@ -8,7 +8,17 @@
             <div class="ne-sub-header ne-sub-header__nutriente">
                 <div class="section-normal">
                     <h1 class="ne-sub-header__title"><?php the_title(); ?></h1>
-                    <p class="ne-sub-header__category">Define category here</p>
+                    <p class="ne-sub-header__category">
+                        <?php
+                        
+                        // get custom taxonomy link and name from 'categoria_alimento'
+                        $terms = wp_get_post_terms( $post->ID, 'categoria_nutriente');
+                        foreach ($terms as $term) {
+                            echo '<a class="ne-link--white" href="'.get_term_link($term->slug, 'categoria_nutriente').'">'.$term->name.'</a>';
+                        }
+                        
+                        ?>
+                    </p>
                 </div>
             </div>
             <div class="section-normal breadcrumb-container">
@@ -61,46 +71,60 @@
                     <aside class="column-with-sidebar__sidebar">
                         <img src="<?php echo get_template_directory_uri(); ?>/img/nead.png" alt="custom ad">
                         <section class="ne-aside__container">
-                            <h3 class="ne-aside__title">Artigos Recentes</h3>
                             <?php
-                                $homePagePosts = new WP_Query(array(
-                                    'posts_per_page' => 4,
-                                    'post__not_in' => array( $post->ID )
-                                ));
-
-                                while ($homePagePosts->have_posts()) {
-                                    $homePagePosts->the_post(); ?>
-                                    <div class="ne-recent-post-item">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php the_post_thumbnail(); ?>
-                                        </a>
-                                        <div class="ne-recent-post-item__details">
-                                            <a class="ne-recent-post-item__title-link" href="<?php the_permalink(); ?>">
-                                                <h4 class="ne-recent-post-item__title"><?php the_title(); ?></h4>
-                                            </a>
-                                            <p class="ne-recent-post-item__date"><?php the_time('j \d\e F, Y'); ?></p>
-                                        </div>
+                                // get the custom post type's taxonomy terms
+                                $custom_taxterms = wp_get_object_terms( $post->ID, 'categoria_nutriente', array('fields' => 'ids') );
+                        
+                                $args = array(
+                                    'post_type' => 'nutriente',
+                                    'post_status' => 'publish',
+                                    'posts_per_page' => 5,
+                                    'post__not_in' => array ( $post->ID ),
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'categoria_nutriente',
+                                            'field' => 'id',
+                                            'terms' => $custom_taxterms
+                                        )
+                                    )
+                                );
+                                $related_items = new WP_Query( $args );
+                                // loop over query
+                                if ( $related_items->have_posts() ) : ?>
+                                    <div class="ne-aside__border-b">
+                                        <h3 class="ne-aside__title">Mais <?php
+                        
+                                        // get custom taxonomy link and name from 'categoria_alimento'
+                                        $terms = wp_get_post_terms( $post->ID, 'categoria_nutriente');
+                                        foreach ($terms as $term) {
+                                            echo $term->name;
+                                        }
+                                        
+                                        ?></h3>
                                     </div>
-                                <?php } wp_reset_postdata();
+                        
+                                    <?php while ( $related_items->have_posts() ) : $related_items->the_post(); ?>
+                                    
+                                        <div class="ne-recent-post-item">
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php the_post_thumbnail(); ?>
+                                            </a>
+                                            <div class="ne-recent-post-item__details">
+                                                <a class="ne-recent-post-item__title-link" href="<?php the_permalink(); ?>">
+                                                    <h4 class="ne-recent-post-item__title"><?php the_title(); ?></h4>
+                                                </a>
+                                                <p class="ne-recent-post-item__date">
+                                                    <?php the_terms( $post->ID, 'categoria_nutriente' ); ?>
+                                                </p>
+                                            </div>
+                                        </div>
+                        
+                                    <?php endwhile; ?>
+                                <?php endif;
+                                // Reset Post Data
+                                wp_reset_postdata();
                             ?>
                         </section>
-                        
-                        <?php
-                            $categories = get_categories();
-
-                            if ($categories) { ?>
-                                <section class="ne-aside__container">
-                                    <h3 class="ne-aside__title">Categorias</h3>
-                                    <ul class="ne-categories-list">
-                                        <?php
-                                            foreach($categories as $category) {
-                                                echo '<li><a class="ne-categories-list__link" href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
-                                            }
-                                        ?>
-                                    </ul>
-                                </section>
-                            <?php }
-                        ?>
                     </aside>
                 </div>
             </div>
