586a62c5a07293fe1348fb9745c2d2ea89fdd8bd
Nuno Machado <nunom4chado@gmail.com>
2019-08-01 15:53:46 +0100

    adds all about nutrition section to home page

--- front-page.php
+++ front-page.php
@@ -13,25 +13,46 @@
                 </div>
             </div>
         </section>
+
+        <section class="section-normal text-center all-about-nutrition">
+            <h2>Tudo sobre nutrição</h2>
+            <div class="all-about-nutrition__inner">
+                <div class="all-about-nutrition__card">
+                    <img class="all-about-nutrition__img" src="<?php echo get_template_directory_uri(); ?>/img/all-about--article.svg" alt="">
+                    <h3 class="all-about-nutrition__card-title">Artigos</h3>
+                    <p class="all-about-nutrition__card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet, nunc et accumsan cursus, neque eros sodales</p>
+                </div>
+                <div class="all-about-nutrition__card">
+                    <img class="all-about-nutrition__img" src="<?php echo get_template_directory_uri(); ?>/img/all-about--article.svg" alt="">
+                    <h3 class="all-about-nutrition__card-title">Alimentos</h3>
+                    <p class="all-about-nutrition__card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet, nunc et accumsan cursus, neque eros sodales</p>
+                </div>
+                <div class="all-about-nutrition__card">
+                    <img class="all-about-nutrition__img" src="<?php echo get_template_directory_uri(); ?>/img/all-about--article.svg" alt="">
+                    <h3 class="all-about-nutrition__card-title">Nutrientes</h3>
+                    <p class="all-about-nutrition__card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet, nunc et accumsan cursus, neque eros sodales</p>
+                </div>
+            </div>
+        </section>
         
 
 
-        <div class="section-normal">
+        <section class="section-normal">
 
-        <?php
-            $homePagePosts = new WP_Query(array(
-                'posts_per_page' => 2
-            ));
+            <?php
+                $homePagePosts = new WP_Query(array(
+                    'posts_per_page' => 2
+                ));
 
-            while ($homePagePosts->have_posts()) {
-                $homePagePosts->the_post(); ?>
-                <h2><?php the_title(); ?></h2>
-                <!-- limit content to 5 words -->
-                <p><?php echo wp_trim_words(get_the_content(), 5); ?></p>
-            <?php } wp_reset_postdata();
-        ?>
+                while ($homePagePosts->have_posts()) {
+                    $homePagePosts->the_post(); ?>
+                    <h2><?php the_title(); ?></h2>
+                    <!-- limit content to 5 words -->
+                    <p><?php echo wp_trim_words(get_the_content(), 5); ?></p>
+                <?php } wp_reset_postdata();
+            ?>
 
-        </div>
+        </section>
     </div>
 
 <?php get_footer(); ?>
